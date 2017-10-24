60ead7432beed89d57b87545fef75f193ec78ae0
Wendy Walsh <Wendy@Roberts-MacBook-Air.local>
2017-10-24 18:15:29 -0500

    Added revenue list

--- PyBank/main2.py
+++ PyBank/main2.py
@@ -2,36 +2,41 @@
 import os
 import csv
 
-csvpath = os.path.join('raw_data','budget_data_1.csv')
-with open(csvpath, newline='') as csvfile:
-    csv_data = csv.reader(csvfile, delimiter = ',')
-    next(csv_data)
+                    #csvpath = os.path.join('raw_data','budget_data_1.csv')
+                    #with open(csvpath, newline='') as csvfile:
+                    #csv_data = csv.DictReader(csvfile, delimiter = ',')
+ 
+
+ with open('raw_data','budget_data_1.csv') as cvsfile:
+    cvsreader = csv.DictReader(cvsfile)
+
+    for row in reader:
+        print(row["Revenue"], row["Date"])
+
 #Store csv as lists
     data = list(csv_data) 
-    
+    print(data)
 #count rows to find total months and set to varialbe -1
     count_data = len(data) 
     total_months = count_data - 1  # -1 due to column header
     
 
 #Create list for Revenue only
-    revenue_list = [item[1] for item in data]
-    print(revenue_list) #works
-
+for row in data:
+    revenue_list = data[1]
+    print(revenue_list)
     #Sum Revenue list set variable as Total_Revenue
-    revenue_sum = sum(revenue_list)
-    print(revenue_sum)
 
 #What is the average revenue change
 
 
 
 # find greatest increase in revenue.  max()
-    max_increase = max(data)
-    #print(max_increase)
+#max_increase = max(data)
+#print(max_increase)
 # find greated decrease in revenue. min()
-    min_increase = min(data)
-    #print(min_increase)
+#min_increase = min(data)
+#print(min_increase)
 
 #Final output
 #print("Financial Analysis")
