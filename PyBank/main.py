dc1534f43c2ef5b427913d59a57fa9baee8cca21
Wendy Walsh <Wendy@roberts-air.lan>
2017-10-25 21:11:00 -0500

    Added PyPoll start

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,9 +5,14 @@ import csv
 csvpath = os.path.join('raw_data','budget_data_1.csv')
 with open(csvpath, newline='') as csvfile:
     csv_data = csv.reader(csvfile, delimiter = ',')
+    #my_list = [[int(x) for x in rec] for rec in csv.reader(p, delimiter=',')]
     next(csv_data)
 #Store csv as lists
-    data = list(csv_data) 
+    data = list(csv_data)
+    print(data)
+
+
+ # change string list to int
     
 #count rows to find total months and set to varialbe -1
     count_data = len(data) 
@@ -17,21 +22,21 @@ with open(csvpath, newline='') as csvfile:
 #Create list for Revenue only
     revenue_list = [item[1] for item in data]
     print(revenue_list) #works
-
+#------------------------------------------------------don't mess with above
     #Sum Revenue list set variable as Total_Revenue
-    revenue_sum = sum(revenue_list)
-    print(revenue_sum)
+    total_revenue = sum(revenue_list)
+   
 
 #What is the average revenue change
 
 
 
 # find greatest increase in revenue.  max()
-    max_increase = max(data)
-    #print(max_increase)
+    max_increase = max(revenue_list)
+    print(max_increase)
 # find greated decrease in revenue. min()
-    min_increase = min(data)
-    #print(min_increase)
+    min_increase = min(revenue_list)
+    print(min_increase)
 
 #Final output
 #print("Financial Analysis")
@@ -41,3 +46,7 @@ with open(csvpath, newline='') as csvfile:
 #print(f"Average Revenue Changes: "{})
 #print(f"Greatest Increase in Revenue: "{max_increase})
 #print(f"Greatest Decerase in Revenue: "{})
+
+#csvpath = os.path.join('raw_data','budget_data_1.csv')
+#with open(csvpath, newline='') as csvfile:
+    #csv_data = csv.reader(csvfile, delimiter = ',')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,71 @@
-PyPoll
+#PyPoll
+
+#Import CSV
+import os
+import csv
+
+#read CSV file to pytho
+
+csvpath = os.path.join('raw_data','election_data_2.csv')
+with open(csvpath, newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =',')
+    next(csvreader)
+    data = list(csvreader)
+#---------------------------------------------------------- works
+
+#Complete list of candidates who received votes
+   #  create empty list for canditates. 
+
+candidate_list = []
+candidate_count = [1,1,1,1,1,1,1,1,1,1] # add count according to candidate list index. 
+                                        #Start with 1 as initial grab of candidate not counted.
+
+   # grab candidate list. Iterate through each row. 
+for row in data:
+
+   # When name found that does not match running list add to  list of candidates.
+    if row != candidate_list[]:
+        candidate_list.append
+        print(candidate_list)
+
+#Count votes per candidate
+   # If name matches list, add to count. 
+        if row == candidate_list[]:
+            candidate_count [] = candidate_list []
+            candidate_count= candidate_count[]+1
+
+
+
+#Total number of votes cast
+    #count total rows - headers
+
+# % of votes each candidate won
+   # count per candidate/total count
+
+
+#zip list for candidate name, percent votes, and total votes
+
+# winner of election based on popular vote
+    #from zip list select max count and store as winner.
+#print outs
+    #print("Election Results")
+    #print("-------------------------------------")
+    #print("Total Videos: " {})
+    #print("-------------------------------------")
+    #print(Zip list)
+    #print("-------------------------------------")
+    #print({winner})
+
+#Final output CSV path
+#output_file = os.path.join("web_final2.csv")
+
+#  Open the output file
+#with open(output_file, "w", newline="") as datafile:
+   # writer = csv.writer(datafile)
+
+    # Write the header row
+    #writer.writerow(["Title", "Course Price", "Subscribers", "Reviews Left",
+                    # "Percent of Reviews", "Length of Course"])
+
+    # Write in zipped rows
+    #writer.writerows(cleaned_csv)
